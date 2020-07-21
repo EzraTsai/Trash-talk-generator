a76fbc9d4c6903cbf45f35353ab8e845f09ca7e6
Ezra Tsai <ishow8166@icloud.com>
2020-07-21 17:11:37 +0800

    setting body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -7,9 +8,17 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+//setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //route setting
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/', (req, res) => {
+    res.render('index')
+    console.log('req.body', req.body)
 })
 
 //start express server and listening for connections
