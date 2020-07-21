dfdcdf24e5cfd79c00f0f0d2e544e8a9d3cd7d51
Ezra Tsai <ishow8166@icloud.com>
2020-07-21 21:57:23 +0800

    add generate_trashtalk to app.js

--- generate_trashtalk.js
+++ generate_trashtalk.js
@@ -1,5 +1,5 @@
 //define generateTrashtalk function
-function generateTrashtalk() {
+function generateTrashtalk(options) {
     // define things user might want
     const task = {
         engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
@@ -7,12 +7,6 @@ function generateTrashtalk() {
         entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
     }
     const phrase = ['很簡單', '很容易', '很快', '很正常']
-
-    //假的取值資料
-    const options = {
-        occupation: 'engineer'
-    }
-
     //比對取值是否相同
     let trashtalk = ''
     for (let prop in task) {
@@ -20,12 +14,21 @@ function generateTrashtalk() {
             const taskIndex = Math.floor(Math.random() * task[prop].length)
             const phraseIndex = Math.floor(Math.random() * phrase.length)
             trashtalk += `身為一個${prop}，${task[prop][taskIndex]}，${phrase[phraseIndex]}吧!`
-            console.log(trashtalk)
         }
     }
+    // return error notice if collection is empty
+    if (trashtalk.length === 0) {
+        return 'There is no valid characters in your selection.'
+    }
     // return the generated trashtalk
-    // return password
+    return trashtalk
 }
+// export generatePassword function for other files to use
+module.exports = generateTrashtalk
 
-//invoke generateTrashtalk function
-generateTrashtalk()
+
+
+
+// if (prop === options.occupation)
+
+// if (prop === Object.keys(options).toString())
