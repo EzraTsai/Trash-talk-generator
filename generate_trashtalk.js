eaca54d319eafb9cff92d221afbfe29deedd5b75
Ezra Tsai <ishow8166@icloud.com>
2020-07-21 23:11:17 +0800

    change for in loop from task to target

--- generate_trashtalk.js
+++ generate_trashtalk.js
@@ -1,6 +1,12 @@
 //define generateTrashtalk function
 function generateTrashtalk(options) {
     // define things user might want
+
+    const target = {
+        engineer: '工程師',
+        designer: '設計師',
+        entrepreneur: '創業家'
+    }
     const task = {
         engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
         designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
@@ -9,11 +15,11 @@ function generateTrashtalk(options) {
     const phrase = ['很簡單', '很容易', '很快', '很正常']
     //比對取值是否相同
     let trashtalk = ''
-    for (let prop in task) {
+    for (let prop in target) {
         if (prop === options.occupation) {
             const taskIndex = Math.floor(Math.random() * task[prop].length)
             const phraseIndex = Math.floor(Math.random() * phrase.length)
-            trashtalk += `身為一個${prop}，${task[prop][taskIndex]}，${phrase[phraseIndex]}吧!`
+            trashtalk += `身為一個${target[prop]}，${task[prop][taskIndex]}，${phrase[phraseIndex]}吧!`
         }
     }
     // return error notice if collection is empty
